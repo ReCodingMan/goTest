05820b333a511e52184097a79653fc04e5e6af0b
chengcheng1021 <chengcheng1021@aliyun.com>
2020-11-17 11:08:36 +0800

    管道断言

---
+++ src/channel/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Cat struct {
+	Name string
+	Age int
+}
+
+func main()  {
+
+	var allChan chan interface{}
+	allChan = make(chan interface{}, 3)
+
+	allChan <- 10
+	allChan <- "tom cat"
+	cat := Cat{"小花猫",4}
+	allChan <- cat
+
+	//直接取第三个
+	<- allChan
+	<- allChan
+	newCat := <- allChan
+
+	fmt.Printf("newCat=%T, newCat=%v \n", newCat, newCat)
+	a := newCat.(Cat)
+	fmt.Printf("newCat.Name=%v", a.Name)
+}
