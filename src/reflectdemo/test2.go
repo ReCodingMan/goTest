9e7066c19bf54df0bf06714d518f07a3d94499bc
chengcheng1021 <chengcheng1021@aliyun.com>
2020-11-18 14:16:19 +0800

    结构体类型反射

--- src/reflectdemo/test2.go
+++ src/reflectdemo/test2.go
@@ -6,7 +6,7 @@ import (
 )
 
 //专门演示反射
-func reflectTest01(b interface{})  {
+func reflectTest02(b interface{})  {
 	//通过反射，获取变量的 type kind 值
 
 	//1、先获取到 reflect.Type
@@ -17,20 +17,32 @@ func reflectTest01(b interface{})  {
 	//2、获取 reflect.Value
 	rVal := reflect.ValueOf(b)
 	fmt.Println("rVal=", rVal)
+	fmt.Println("rVal.Kind=", rVal.Kind())
 	fmt.Printf("rVal=%v, rVal-type=%T\n", rVal, rVal)
 
 	//3、将 rVal 转成 interface{}
 	iv := rVal.Interface()
+	fmt.Printf("iv=%v, iv-type=%T\n", iv, iv)
+
 	//将 Interface{} 通过断言转成需要的类型
-	num2 := iv.(int)
+	num2 := iv.(Student)
 	fmt.Println("num2=", num2)
 
 }
 
+type Student struct {
+	Name string
+	Age int
+}
+
 func main() {
 
-	var num int = 100
-	reflectTest01(num)
+	stu := Student{
+		Name : "tom",
+		Age : 10,
+	}
+
+	reflectTest02(stu)
 }
 
 
