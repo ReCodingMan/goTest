ad84b3fbb6c66f7330c11149a9af3fc7f43ceb13
chengcheng1021 <chengcheng1021@aliyun.com>
2020-11-19 19:58:57 +0800

    redis操作string

--- src/redis/string.go
+++ src/redis/string.go
@@ -7,13 +7,31 @@ import (
 
 func main() {
 
-	//通过go向redis写入数据和读写数据
 	//1、连接redis
 	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
 	if err != nil {
 		fmt.Println("redis.Dial err=", err)
 		return
 	}
-	fmt.Println("redis.Dial suc", conn)
+	defer conn.Close()
+
+	//2、通过go向redis写入数据 string [key-val]
+	_, err = conn.Do("Set", "name", "tomjerry")
+	if err != nil {
+		fmt.Println("set err=", err)
+		return
+	}
+
+	//3、通过go向redis读取数据 string
+	r, err := redis.String(conn.Do("Get", "name"))
+	if err != nil {
+		fmt.Println("get err=", err)
+		return
+	}
+	//因为返回的 r 是 interface{}
+	//因为 name 对应的值是 string，因此我们需要转换
+
+	//nameString := r.(string)  这是错误的
+	fmt.Println("get suc", r)
 
 }
