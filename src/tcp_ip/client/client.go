d6e5d37e6513acba7007c9ec6270f2e1dfaba7fc
chengcheng1021 <chengcheng1021@aliyun.com>
2020-11-19 11:00:46 +0800

    tcp客户端和服务器端完成

--- src/tcp_ip/client/client.go
+++ src/tcp_ip/client/client.go
@@ -1,1 +1,33 @@
-package client
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	conn, err := net.Dial("tcp", "0.0.0.0:8888")
+	if err != nil {
+		fmt.Println("client dial err=", err)
+		return
+	}
+	fmt.Println("conn 成功=", conn)
+
+	//功能一：输出单行数据，然后退出
+	reader := bufio.NewReader(os.Stdin) //标准输入
+	//从终端读取一行用户输入，准备发送服务器
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("ReadString err=", err)
+	}
+
+	//发送给服务器
+	n, err := conn.Write([]byte(line))
+	if err != nil {
+		fmt.Println("conn.Write err=", err)
+	}
+	fmt.Printf("客户端发送了 %d 字节的数据，并退出 \n", n)
+}
--- src/tcp_ip/server/server.go
+++ src/tcp_ip/server/server.go
@@ -5,6 +5,30 @@ import (
 	"net" //做网络socket开发时，net 包含我们需要所有的方法和函数
 )
 
+func process(conn net.Conn) {
+
+	defer conn.Close() // 关闭conn
+
+	for {
+		//创建一个新的切片
+		buf := make([]byte, 1024)
+		//这里循环接收客户端发送的数据
+
+		//conn.Read(buf)
+		//1、等待客户端通过 conn 发送信息
+		//2、如果客户端没有write[发送]，那么协程就阻塞在这里
+		fmt.Printf("服务器在等待客户端%s，发送信息\n", conn.RemoteAddr().String())
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("服务器端read出错", err)
+			return
+		}
+
+		//3、显示客户端发送的数据内容到服务器的终端
+		fmt.Print(string(buf[:n]))
+	}
+}
+
 func main()  {
 
 	fmt.Println("服务器开始监听...")
@@ -24,9 +48,10 @@ func main()  {
 		if err != nil {
 			fmt.Println("Accept() err = ", err)
 		}
-		fmt.Printf("Accept() suc con=%v\n", conn)
+		fmt.Printf("Accept() suc con=%v, 客户端ip=%v \n", conn, conn.RemoteAddr().String())
 
 		//这里起一个协程，为客户端服务
+		go process(conn)
 	}
 	fmt.Printf("listen=%v\n", listen)
 
