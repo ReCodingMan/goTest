87ac5eb0e09cb29f641cca3ee34d88b380c30112
chengcheng1021 <chengcheng1021@aliyun.com>
2020-11-20 15:39:54 +0800

    接收长度

--- src/chatroom/client/login.go
+++ src/chatroom/client/login.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"chatroom/common/message"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"net"
@@ -16,11 +17,13 @@ func login(userId int, userPwd string) (err error) {
 	//return nil
 
 	//1、链接到服务器端
-	conn, err := net.Dial("tcp", "localhost:8889")
+	conn, err := net.Dial("tcp", "0.0.0.0:8889")
 	if err != nil {
 		fmt.Println("net.Dial err=", err)
 		return
 	}
+	//延时关闭
+	defer conn.Close()
 
 	//2、准备通过conn发送消息给服务
 	var mes message.Message
@@ -47,6 +50,17 @@ func login(userId int, userPwd string) (err error) {
 
 	//7、先把data长度发送给服务器
 	//先获取到data的长度->转成一个表示长度的byte切片
+	var pkgLen uint32
+	pkgLen = uint32(len(data))
+	var buf [4]byte
+	binary.BigEndian.PutUint32(buf[0:4], pkgLen)
+	//发送长度
+	n, err := conn.Write(buf[:4])
+	if n != 4 || err != nil {
+		fmt.Println("conn.Write(bytes) fail =", err)
+		return
+	}
 
-
+	fmt.Printf("客户端，发送消息的长度=%d\n", len(data))
+	return
 }
--- src/chatroom/server/main.go
+++ src/chatroom/server/main.go
@@ -8,7 +8,20 @@ import (
 //处理和客户端的通讯
 func process(conn net.Conn) {
 
-	//读客户端发送的信息
+	//延时关闭
+	defer conn.Close()
+
+	//循环读客户端发送的信息
+	for {
+		buf := make([]byte, 8096)
+		fmt.Println("读取客户端发送的数据...")
+		n, err := conn.Read(buf[:4])
+		if n != 4 || err != nil {
+			fmt.Println("conn.Read err", err)
+			return
+		}
+		fmt.Println("读到的buf=", buf[:4])
+	}
 
 }
 
@@ -17,6 +30,7 @@ func main()  {
 	//提示信息
 	fmt.Println("服务器在8889监听...")
 	listen, err := net.Listen("tcp", "0.0.0.0:8889")
+	defer listen.Close()
 	if err != nil {
 		fmt.Println("listen err=", err)
 		return
