f4bc0a1d317d85c256d820db3d9620dbdf3b5aac
chengcheng1021 <chengcheng1021@aliyun.com>
2020-11-20 10:48:18 +0800

    通讯系统初始化

---
+++ src/chatroom/client/login.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+//写一个函数，完成登录
+func login(userId int, userPwd string) (err error) {
+
+	//下一步就要开始定协议
+	fmt.Printf("userId = %d, userPwd = %s\n", userId, userPwd)
+
+	return nil
+}
