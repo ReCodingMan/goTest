b5444ec1a60a134ab3c0958f9fe8c7595f1a7fef
chengcheng1021 <chengcheng1021@aliyun.com>
2020-11-25 16:55:14 +0800

    迷宫实现

--- src/datastruct/migong/main2.go
+++ src/datastruct/migong/main2.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+//编写一个函数，找出路
+func SetWay(myMap *[8][7]int, i int, j int) bool {
+
+	//找到出口
+	if myMap[6][5] == 2 {
+		return true
+	} else {
+		//继续找
+		if myMap[i][j] == 0 {
+			//可以探测，假设这个点通的，但是需要探测。优先级：上下左右
+			myMap[i][j] = 2
+			if SetWay(myMap, i-1, j) {//上
+				return true
+			} else if SetWay(myMap, i+1, j) {//下
+				return true
+			} else if SetWay(myMap, i, j-1) {//左
+				return true
+			} else if SetWay(myMap, i, j+1) {//右
+				return true
+			} else {//死路一条
+				myMap[i][j] = 3
+				return false
+			}
+		} else {
+			//不可以探测
+			return false
+		}
+	}
+}
+
 func main() {
 
 	//1、创建一个二维数组，模拟迷宫
@@ -21,7 +51,9 @@ func main() {
 		myMap[i][0] = 1
 		myMap[i][6] = 1
 	}
-	
+	myMap[3][1] = 1
+	myMap[3][2] = 1
+
 	//打印
 	for i:=0; i<8; i++ {
 		for j:=0; j<7; j++ {
@@ -30,4 +62,14 @@ func main() {
 		fmt.Println()
 	}
 
+	//使用测试
+	SetWay(&myMap, 1, 1)
+
+	//打印
+	for i:=0; i<8; i++ {
+		for j:=0; j<7; j++ {
+			fmt.Print(myMap[i][j], " ")
+		}
+		fmt.Println()
+	}
 }
